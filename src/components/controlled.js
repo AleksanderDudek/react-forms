ca3b5bca897eb7803f32911560db2b0226b98f63
Aleksander Dudek <aleksander.dudek@tieto.com>
2018-08-09 15:03:05 +0200

    uncontrolled added and controlled

--- src/components/controlled.js
+++ src/components/controlled.js
@@ -3,13 +3,55 @@ import React, { Component } from 'react';
 class Controlled extends Component {
 
     state = {
-        
+        name: '',
+        lastname: ''
+    }
+
+    handleLastNameChange = (event) => {
+        console.log(event.target.value)
+        this.setState({
+            lastname:event.target.value
+        })
+    }
+
+    handleNameChange = (event) => {
+        console.log(event.target.value)
+        this.setState({
+            name:event.target.value
+        })
+    }
+
+    onSaveHandler = (event) => {
+        event.preventDefault();
+
+        console.log(this.state)
     }
 
     render(){
         return(
-            <div>
-                Controlled
+            <div className="container">
+                <form onSubmit={this.onSaveHandler}>
+                    <div className="form_element">
+                        <label>Enter name</label>
+                        <input
+                            type="text"
+                            onChange={this.handleNameChange}
+                            value={this.state.name}
+                            placeholder="name..."
+                        />
+                    </div>
+                    <div className="form_element">
+                        <label>Enter last name</label>
+                        <input
+                            type="text"
+                            onChange={this.handleLastNameChange}
+                            value={this.state.lastname}
+                            placeholder="last name..."
+
+                        />
+                    </div>
+                    <button type="submit">Submit</button>
+                </form>
             </div>
         )
     }
--- src/components/uncontrolled.js
+++ src/components/uncontrolled.js
@@ -2,14 +2,45 @@ import React, { Component } from 'react';
 
 class Uncontrolled extends Component {
 
-    state = {
+    handleSubmitClick = (event) => {
+        event.preventDefault();
 
+        const value = {
+            name: this.name.value,
+            lastname: this.lastname.value
+        }
+
+        console.log(value)
     }
 
     render(){
         return(
-            <div>
-                Uncontrolled
+            <div className="container">
+                <form>
+                    <div className="form_element">
+                        <label>Enter name</label>
+                        <input
+                            type="text"
+                            placeholder="name..."
+                            ref={input => this.name = input}
+                        />
+                    </div>
+                    <div className="form_element">
+                        <label>Enter last name</label>
+                        <input
+                            type="text"
+                            placeholder="last name..."
+                            ref={input => this.lastname = input}
+                        />
+                    </div>
+
+                    {/* is button assuming submit control by default in form??? */}
+                    <button
+                        onClick={this.handleSubmitClick}
+                    >
+                        Sign in
+                    </button>
+                </form>
             </div>
         )
     }
